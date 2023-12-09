9a2f9c36bfcf6510c2b7d2a392e4fbe2f7e34bff
IgorL123 <Zedenbraucht@inbox.ru>
2023-12-09 15:55:41 +0300

    bugfix 3

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ from fastapi import FastAPI
 from fastapi_utils.tasks import repeat_every
 from scrappers import NewsRBCScrapper
 from scrappers import CyberScrapper
+import uvicorn
+
 
 app = FastAPI()
 
 PERIOD = 6 * 60 * 60  # 3 times a day
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+TEST = 60
 
 
 def getting_news_urls():
@@ -24,12 +22,18 @@ def getting_papers():
     run.collect()
 
 
-@app.on_event("startup")
-@repeat_every(seconds=PERIOD)
+def test():
+    news = NewsRBCScrapper()
+    news.test_logs()
+
+
+@app.get("/")
+@repeat_every(seconds=TEST)
 def r_task() -> None:
     print(1)
-    getting_news_urls()
-    print(2)
-    getting_papers()
+    test()
     print(3)
 
+
+if __name__ == "__main__":
+    uvicorn.run(app)
